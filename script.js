b6debf3e82092474d0cff9e2dc41a2c2efee905e
pranit <patilpranit3112@gmail.com>
2021-10-22 17:24:48 +0530

    Added Navbar & Login Form

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let searchbtr=document.querySelector('#search-btr');
 let searchbox=document.querySelector('.search-bar-box');
 
+let formbtr=document.querySelector('#login-btr');
+let loginform=document.querySelector('.login-form-container');
+let formClose=document.querySelector('#form-close');
+
 window.onscroll=()=>{
     searchbtr.classList.remove('fa-times');
     searchbox.classList.remove('active');
@@ -9,4 +13,12 @@ window.onscroll=()=>{
 searchbtr.addEventListener('click',()=>{
     searchbtr.classList.toggle('fa-times');
     searchbox.classList.toggle('active');
+})
+
+formbtr.addEventListener('click',()=>{
+    loginform.classList.add('active');
+})
+
+formClose.addEventListener('click',()=>{
+    loginform.classList.remove('active');
 })
