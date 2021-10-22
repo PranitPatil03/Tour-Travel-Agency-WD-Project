01babc1c96a8fb9fafd9890d236e72b5bbddbdd6
pranit <patilpranit3112@gmail.com>
2021-10-22 19:10:03 +0530

    Header+Navg+Login Done

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ let formbtr=document.querySelector('#login-btr');
 let loginform=document.querySelector('.login-form-container');
 let formClose=document.querySelector('#form-close');
 
+let menu=document.querySelector('#menu-bar');
+let navbar=document.querySelector('.navbar');
+
 window.onscroll=()=>{
     searchbtr.classList.remove('fa-times');
     searchbox.classList.remove('active');
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    loginform.classList.remove('active');    
 }
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+})
 
 searchbtr.addEventListener('click',()=>{
     searchbtr.classList.toggle('fa-times');
